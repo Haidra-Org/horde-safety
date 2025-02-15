1c7a33abf0395a3fcb725fea18469627e6bd3af2
tazlin <tazlin.on.github@gmail.com>
2025-02-15 08:45:49 -0500

    fix: support HF_HOME

--- horde_safety/__init__.py
+++ horde_safety/__init__.py
@@ -2,26 +2,30 @@
 
 # flake8: noqa
 import os
-
 from loguru import logger
 
-CACHE_FOLDER_PATH: str
+CACHE_FOLDER_PATH: str = os.path.expanduser("~/.cache/huggingface/")
 
 AIWORKER_CACHE_HOME = os.getenv("AIWORKER_CACHE_HOME")
+TRANSFORMERS_CACHE = os.getenv("TRANSFORMERS_CACHE")
+HF_HOME = os.getenv("HF_HOME")
 
 if AIWORKER_CACHE_HOME:
-    CACHE_FOLDER_PATH = os.path.join(AIWORKER_CACHE_HOME, "clip_blip")
-    if os.getenv("TRANSFORMERS_CACHE") is None:
-        os.environ["TRANSFORMERS_CACHE"] = AIWORKER_CACHE_HOME + "/hf_transformers"
+    if not TRANSFORMERS_CACHE and not HF_HOME:
+        CACHE_FOLDER_PATH = os.path.join(AIWORKER_CACHE_HOME, "clip_blip")
+        os.environ["HF_HOME"] = os.path.join(AIWORKER_CACHE_HOME, "hf_transformers")
     else:
-        logger.info("TRANSFORMERS_CACHE already set, not overriding")
+        logger.info("TRANSFORMERS_CACHE or HF_HOME already set, not overriding")
 else:
     logger.info("AIWORKER_CACHE_HOME not set, using default huggingface cache paths.")
-    if os.getenv("TRANSFORMERS_CACHE") is None:
-        CACHE_FOLDER_PATH = "~/.cache/huggingface/"
+    if HF_HOME:
+        CACHE_FOLDER_PATH = HF_HOME
+        logger.debug(f"Using HF_HOME: {HF_HOME}")
+    elif TRANSFORMERS_CACHE:
+        CACHE_FOLDER_PATH = TRANSFORMERS_CACHE
+        logger.debug(f"Using TRANSFORMERS_CACHE: {TRANSFORMERS_CACHE}")
     else:
-        CACHE_FOLDER_PATH = os.getenv("TRANSFORMERS_CACHE", "~/.cache/huggingface/")
-
+        logger.debug(f"Using default cache path: {CACHE_FOLDER_PATH}")
 
 from horde_safety.interrogate import get_interrogator_no_blip, CAPTION_MODELS
 from horde_safety.csam_checker import check_for_csam
